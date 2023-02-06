f6f15b46bd04d68df8040f8486f7edddebf68079
iamaamunir <abdullahimunir3@gmail.com>
2023-02-06 10:06:45 +0100

    fix:error handling on routes without handlers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const AppError = require("./utils/appError");
 const CONFIG = require("./config/config");
 const NODE_ENV = CONFIG.NODE_ENV;
 const app = express();
@@ -32,5 +33,21 @@ app.use((req, res, next) => {
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+//  Handling routes without handlers
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't access ${req.originalUrl} on this server`, 404));
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+  next();
+});
 
 module.exports = app;
