bceab2b252f257b6969c0e2c88c954eaeec63311
iamaamunir <abdullahimunir3@gmail.com>
2023-02-15 18:46:49 +0100

    fix: uncaught rejection error

--- server.js
+++ server.js
@@ -8,12 +8,19 @@ connectToDb();
 
 const port = CONFIG.PORT || 3000;
 
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception... Server is shutting down");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const server = app.listen(port, () => {
   console.log(`App is listening from port ${port}`);
 });
 
 process.on("unhandledRejection", (err) => {
   console.log("Unhandled Rejection... Server is shutting down");
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
