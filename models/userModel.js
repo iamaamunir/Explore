0639be3cbd4c7499831a36961448adeb2653b275
iamaamunir <abdullahimunir3@gmail.com>
2023-02-16 14:02:48 +0100

    fix: user model created

---
+++ models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const userModel = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please tell us your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    validate: [validator.isEmail, "Please provide a valid email address"],
+    unique: true,
+    lowercase: true,
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a valid password"],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
