34117b7492f79f55ec70a313bece9d5a8877dae2
Munir Abdullahi <abdullahimunir3@gmail.com>
2023-02-24 02:16:24 +0100

    added jwt related properties

--- config/config.js
+++ config/config.js
@@ -4,6 +4,8 @@ const CONFIG = {
     NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
   MONGODB_URL: process.env.MONGODB_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN:process.env.JWT_EXPIRES_IN
 };
 
 module.exports = CONFIG;
